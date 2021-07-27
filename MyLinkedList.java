c61131ba10b7d1be3fc9863dfe348a2a7fa3b4e0
dmmaks <54597096+dmmaks@users.noreply.github.com>
2021-07-27 21:32:42 +0300

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,36 +1,30 @@
 public class MyLinkedList<T> {
-    Node head;
-    Node tail;
-    int size;
+    private Node head;
+    private Node tail;
+    private int size;
 
-    public MyLinkedList()
-    {
+    public MyLinkedList() {
         head = null;
         tail = null;
         size = 0;
     }
 
-    public void print()
-    {
+    public void print() {
         System.out.print("LinkedList contents: ");
         Node current = head;
-        for (int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < size; i++) {
             System.out.print(current.getValue() + " ");
             current = current.getNext();
         }
         System.out.print("\n");
     }
 
-    public void add(T value)
-    {
-        if (head == null)
-        {
+    public void add(T value) {
+        if (head == null) {
             this.head = new Node<T>(value);
             tail = head;
         }
-        else
-        {
+        else {
             tail.setNext(new Node<T>(value));
             tail.getNext().setPrev(tail);
             tail = tail.getNext();
@@ -38,56 +32,45 @@ public class MyLinkedList<T> {
         size++;
     }
 
-    public void remove(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
             return;
         }
-        if (index == 0)
-        {
+        if (index == 0) {
             head = head.getNext();
             head.setPrev(null);
             size--;
             return;
         }
         Node searched = head;
-        for (int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             searched = searched.getNext();
         }
         searched.getPrev().setNext(searched.getNext());
-        if (searched.getNext() != null)
-        {
+        if (searched.getNext() != null) {
             searched.getNext().setPrev(searched.getPrev());
         }
-        else
-        {
+        else {
             tail = searched.getPrev();
         }
         size--;
     }
 
-    public void clear()
-    {
+    public void clear() {
         size = 0;
         head = null;
     }
 
-    public int size()
-    {
+    public int size() {
         return size;
     }
 
-    public T get(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
         Node searched = head;
-        for (int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             searched = searched.getNext();
         }
         return (T) searched.getValue();
