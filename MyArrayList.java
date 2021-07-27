af5649f4f6d3404b23982a4d9e7d3301f4d9a154
dmmaks <54597096+dmmaks@users.noreply.github.com>
2021-07-27 21:33:09 +0300

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -3,62 +3,51 @@ public class MyArrayList<T> {
     private int capacity;
     private int size;
 
-    public MyArrayList()
-    {
+    public MyArrayList() {
         arr = (T[]) new Object[5];
         capacity = 5;
         size = 0;
     }
 
-    public void print()
-    {
+    public void print() {
         System.out.print("ArrayList contents: ");
-        for (int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.print("\n");
     }
 
-    public void resize()
-    {
+    public void resize() {
         T[] new_arr = (T[]) new Object[capacity * 2];
         System.arraycopy(arr, 0, new_arr, 0, arr.length);
         this.arr = new_arr;
         capacity *= 2;
     }
 
-    public void add(T value)
-    {
-        if (size == capacity)
-        {
+    public void add(T value) {
+        if (size == capacity) {
             this.resize();
         }
         arr[size] = value;
         size++;
     }
 
-    public void remove(int index)
-    {
-        for (int i = index; i < size - 1; i++)
-        {
+    public void remove(int index) {
+        for (int i = index; i < size - 1; i++) {
             arr[i] = arr[i + 1];
         }
         size--;
     }
 
-    public void clear()
-    {
+    public void clear() {
         size = 0;
     }
 
-    public int size()
-    {
+    public int size() {
         return size;
     }
 
-    public T get(int index)
-    {
+    public T get(int index) {
         if (index >= 0 && index < size)
             return arr[index];
         return null;
