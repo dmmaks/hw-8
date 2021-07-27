d26765b4da1a448601358c70d5365cfaa12d30e3
dmmaks <54597096+dmmaks@users.noreply.github.com>
2021-07-27 21:31:34 +0300

    Update SingleLinkNode.java

--- SingleLinkNode.java
+++ SingleLinkNode.java
@@ -3,45 +3,37 @@ public class SingleLinkNode<K ,V> {
     private V value;
     private SingleLinkNode next;
 
-    public SingleLinkNode()
-    {
+    public SingleLinkNode() {
         next = null;
     }
 
-    public SingleLinkNode(K key, V value)
-    {
+    public SingleLinkNode(K key, V value) {
         this();
         this.key = key;
         this.value = value;
     }
 
-    public K getKey()
-    {
+    public K getKey() {
         return key;
     }
 
-    public void setKey(K key)
-    {
+    public void setKey(K key) {
         this.key = key;
     }
 
-    public V getValue()
-    {
+    public V getValue() {
         return value;
     }
 
-    public void setValue(V value)
-    {
+    public void setValue(V value) {
         this.value = value;
     }
 
-    public SingleLinkNode getNext()
-    {
+    public SingleLinkNode getNext() {
         return next;
     }
 
-    public void setNext(SingleLinkNode next)
-    {
+    public void setNext(SingleLinkNode next) {
         this.next = next;
     }
 }
